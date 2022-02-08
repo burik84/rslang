9a138c7179b6d474700a0a7446130a2c426e2d98
burik84 <avburik84@gmail.com>
2022-02-08 21:27:35 +0300

    refactor: change name storage to team

--- src/scripts/shared/localstorage.ts
+++ src/scripts/shared/localstorage.ts
@@ -1,20 +1,20 @@
 
 const setValue = (value, key='user') => {
-    const name=`burik84-${key}`;
+    const name=`rsteam17-${key}`;
     const valueJSON=JSON.stringify(value);
 
     localStorage.setItem(name, valueJSON);
 };
 
 const getValue = (key='user') => {
-    const name=`burik84-${key}`;
+    const name=`rsteam17-${key}`;
     const value = JSON.parse(localStorage.getItem(name));
 
     return value;
 };
 
 const resetValue = (key='user') => {
-    const name=`burik84-${key}`;
+    const name=`rsteam17-${key}`;
     localStorage.removeItem(name);
 };
 export { setValue, getValue,  resetValue };
