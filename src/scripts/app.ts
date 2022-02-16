8f5cfb29c7fafe540352339c51d26b33e4b22f9f
lg0leg <lg.oleg1067@gmail.com>
2022-02-16 21:43:05 +0300

    feat: add chart.js

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -2,12 +2,14 @@ import { servicesApi } from './shared/services';
 import { audioGame } from './games/audioGame';
 import { controllers } from './controllers/controller';
 import { nav } from './view/components/nav';
+import { statistics } from './view/components/statistics';
 
 const app = {
   start: () => {
     console.log('Start App');
     nav();
     audioGame();
+    statistics();
     controllers.init();
   },
   testSevice: () => {
