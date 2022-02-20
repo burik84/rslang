b7bf7e49c181a8e0cfbe9e194dd3298ee81a7d71
Blackdoll-Loki <helpmeloki7@gmail.com>
2022-02-20 02:54:15 +0200

    feat: add ts for sprint

---
+++ src/scripts/games/sprintGame.ts
@@ -0,0 +1,18 @@
+const lvla1: HTMLUListElement = document.querySelector('#lvla1');
+const lvla2: HTMLUListElement = document.querySelector('#lvla2');
+const lvlb1: HTMLUListElement = document.querySelector('#lvlb1');
+const lvlb2: HTMLUListElement = document.querySelector('#lvlb2');
+const lvlc1: HTMLUListElement = document.querySelector('#lvlc1');
+const lvlc2: HTMLUListElement = document.querySelector('#lvlc2');
+
+const gameStartPage: Element = document.querySelector('.sprint-game-start');
+const gamePlayPage: Element = document.querySelector('.sprint-game-play');
+const lvlBtn: HTMLButtonElement = document.querySelector('#lvl-btn');
+
+lvlBtn.addEventListener('click', function(){
+  console.log(gamePlayPage.classList);
+  gamePlayPage.classList.toggle('.visually-hidden');
+
+  gameStartPage.classList.toggle('.visually-hidden');
+  console.log('click');
+});
