1e07172790a99ae8129370e125b7fcdd16008e18
Blackdoll-Loki <helpmeloki7@gmail.com>
2022-02-20 03:35:10 +0200

    fix: add function export

--- src/scripts/games/sprintGame.ts
+++ src/scripts/games/sprintGame.ts
@@ -1,20 +1,23 @@
-const lvla1: HTMLUListElement = document.querySelector('#lvla1');
-const lvla2: HTMLUListElement = document.querySelector('#lvla2');
-const lvlb1: HTMLUListElement = document.querySelector('#lvlb1');
-const lvlb2: HTMLUListElement = document.querySelector('#lvlb2');
-const lvlc1: HTMLUListElement = document.querySelector('#lvlc1');
-const lvlc2: HTMLUListElement = document.querySelector('#lvlc2');
+function sprintGame(){
+  const lvla1: HTMLUListElement = document.querySelector('#lvla1');
+  const lvla2: HTMLUListElement = document.querySelector('#lvla2');
+  const lvlb1: HTMLUListElement = document.querySelector('#lvlb1');
+  const lvlb2: HTMLUListElement = document.querySelector('#lvlb2');
+  const lvlc1: HTMLUListElement = document.querySelector('#lvlc1');
+  const lvlc2: HTMLUListElement = document.querySelector('#lvlc2');
 
-const gameStartPage: Element = document.querySelector('.sprint-game-start');
-const gamePlayPage: Element = document.querySelector('.sprint-game-play');
-const lvlBtn: HTMLButtonElement = document.querySelector('#lvl-btn');
+  const gameStartPage: Element = document.querySelector('.sprint-game-start');
+  const gamePlayPage: Element = document.querySelector('.sprint-game-play');
+  const lvlBtn: HTMLButtonElement = document.querySelector('#lvl-btn');
 
-lvlBtn.addEventListener('click', function(){
-  console.log(gamePlayPage.classList);
-  gamePlayPage.classList.toggle('.visually-hidden');
+  lvlBtn.addEventListener('click', function(){
+    console.log(gamePlayPage.classList);
+    gamePlayPage.classList.toggle('.visually-hidden');
 
-  gameStartPage.classList.toggle('.visually-hidden');
-  console.log('click');
-});
+    gameStartPage.classList.toggle('.visually-hidden');
+    console.log('click');
+  });
+}
 
-export { lvlBtn }
+
+export { sprintGame }
