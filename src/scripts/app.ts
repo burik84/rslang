6e6d4554e028b681ec9c9dce7e84abe241557404
Blackdoll-Loki <helpmeloki7@gmail.com>
2022-02-20 22:58:29 +0200

    fix: import sprintGame to app.ts

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -3,6 +3,7 @@ import { audioGame } from './games/audioGame';
 import { controllers } from './controllers/controller';
 import { nav } from './view/components/nav';
 import { statistics, statisticsChart } from './view/components/statistics';
+import { sprintGame } from './games/sprintGame'
 
 const app = {
   start: () => {
